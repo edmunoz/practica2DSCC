3225a1b440da00e381c9b6d5483041465fbbd86f
unknown <labproto1@cti_thin_53_en.cti.espol.edu.ec>
2015-11-09 17:08:39 -0500

    Implementacion para tomar foto y guardarla en la SDCard

--- GadgeteerPractica2/Program.cs
+++ GadgeteerPractica2/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.SPOT.Presentation.Controls;
 using Microsoft.SPOT.Presentation.Media;
 using Microsoft.SPOT.Presentation.Shapes;
 using Microsoft.SPOT.Touch;
+using System.IO;
 
 using Gadgeteer.Networking;
 using GT = Gadgeteer;
@@ -17,6 +18,7 @@ namespace GadgeteerPractica2
 {
     public partial class Program
     {
+        GT.Timer timer = new GT.Timer(1500,GT.Timer.BehaviorType.RunOnce);
         // This method is run when the mainboard is powered up or reset.   
         void ProgramStarted()
         {
@@ -40,10 +42,43 @@ namespace GadgeteerPractica2
             Debug.Print("Program Started");
             camera.BitmapStreamed += camera_BitmapStreamed;
             camera.CameraConnected += camera_CameraConnected;
-            //camera.StartStreaming();
-            //this.button.ButtonPressed += button_ButtonPressed;  
-           
+            button.ButtonPressed += button_ButtonPressed;
+            camera.PictureCaptured += camera_PictureCaptured;
+            sdCard.Mounted += sdCard_Mounted;
+            //camera.StartStreaming();            
+            timer.Tick += timer_Tick;   
         }
+
+        void timer_Tick(GT.Timer timer)
+        {
+            camera.StartStreaming();
+        }
+
+
+
+        void sdCard_Mounted(SDCard sender, GT.StorageDevice device)
+        {
+            //sdCard.Mount();
+            Debug.Print("Mount");
+        }
+
+        void camera_PictureCaptured(Camera sender, GT.Picture e)
+        {
+            //displayT35.SimpleGraphics.DisplayImage(e, 0, 0);
+            if (sdCard.IsCardMounted)
+            {
+                button.TurnLedOn();
+                sdCard.StorageDevice.WriteFile("foto.bmp", e.PictureData);
+                button.TurnLedOff();
+                Debug.Print("Save picture");
+            }
+            else
+                Debug.Print("Not CardInserted");
+
+            timer.Start();
+        }
+        
+
 
         void camera_CameraConnected(Camera sender, EventArgs e)
         {
@@ -53,13 +88,16 @@ namespace GadgeteerPractica2
 
         void button_ButtonPressed(Button sender, Button.ButtonState state)
         {
-            //this.camera.StartStreaming();
+            camera.StopStreaming();
+            camera.TakePicture();
         }
 
         void camera_BitmapStreamed(Camera sender, Bitmap e)
         {           
             displayT35.SimpleGraphics.DisplayImage(e,0,0);
-            //this.camera.StopStreaming();
+            
         }
+
+
     }
 }
